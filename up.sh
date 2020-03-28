ff802efd64d28a3cba6055e22300df0682ae9780
Massimo Lizza <massimo.lizza@skytv.it>
2020-03-28 16:30:17 +0100

    up.sh script

--- up.sh
+++ up.sh
@@ -2,6 +2,8 @@
 set -ex
 
 rm -rf base
-git clone https://github.com/picostack/pico-example-target.git base
+git clone https://github.com/mlizza/pico-example-target.git base
+cd base
+git checkout tags/1.0.0 -b 1.0.0
 
-docker-compose -f base/docker-compose.yml -f docker-compose.yml up -d
+#docker-compose -f base/docker-compose.yml -f docker-compose.yml up -d
